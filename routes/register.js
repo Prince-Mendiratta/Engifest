5800104f385b9c2ebf0296640b982729852b29ab
Prince Mendiratta <prince.mendi@gmail.com>
2021-12-20 11:50:35 +0530

    Setup pages and db
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

---
+++ routes/register.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var alert = [{
+    msg: "bruh test"
+  }, {
+    msg: "Not cool"
+  }]
+  res.render('register', {alert});
+  // res.render('register');
+});
+
+module.exports = router;
